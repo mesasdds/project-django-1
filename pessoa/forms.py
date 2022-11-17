64619564bf9281ba50406c978a819f92bc0a603b
mesasdds <mesasdds@gmail.com>
2022-11-17 13:43:25 -0300

    adicao/delete e filtros de nomes na tabela

---
+++ pessoa/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Pessoa
+
+class PessoaForm(forms.ModelForm):
+    data_nascimento = forms.DateField(
+        widget=forms.TextInput(
+            attrs={"type":"date"}
+        )
+    )
+    class Meta:
+        model = Pessoa
+        fields = ['nome_completo', 'data_nascimento', 'ativa']
